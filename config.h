786a80dffd940b0fe7994972f1b385153d1814ac
kofjoker <kofjoker@gmail.com>
2021-06-10 21:31:56 +0800

    mod+g exec chromium,mod+c exec brave

--- config.h
+++ config.h
@@ -64,8 +64,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *broswer1[]  = { "brave", NULL };
-static const char *broswer2[]  = { "chromium", NULL };
+static const char *broswer1[]  = { "chromium", NULL };
+static const char *broswer2[]  = { "brave", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 
